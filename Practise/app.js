aa76c0198f194e0ad1c8e46a4abf3a8577064fab
Rahul Reddy Chavva <chavva@usf.edu>
2021-07-15 12:49:05 -0400

    Moving from Master branch to main

---
+++ Practise/app.js
@@ -0,0 +1,40 @@
+//alert("hello world");
+
+var name = 'John Doe';
+console.log(name);
+name = 'Steve smith';
+console.log(name);
+var greeting;
+
+let sample = 'John';
+console.log(sample);
+sample = 'Steve';
+console.log(sample);
+
+const age = 45;
+console.log(age);
+
+const hasKids = true;
+console.log(hasKids);
+
+const notDefined = undefined;
+console.log(notDefined);
+
+const returnNull = null;
+console.log(returnNull);
+
+const sym = Symbol();
+console.log(sym);
+
+const today = new Date();
+console.log(today);
+
+let val;
+val = 5;
+console.log(val);
+console.log(typeof val);
+console.log(val.length == undefined);
+
+val = String(val);
+console.log(typeof val);
+console.log(val);
---
+++ Practise/function_declaration.js
@@ -0,0 +1,5 @@
+function greet(name){
+    //console.log('Hello');
+    return `Hello ${name}`;
+}
+console.log(greet("Soujanya"));
