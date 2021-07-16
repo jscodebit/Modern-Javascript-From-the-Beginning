be3981759a11cfef6daa4b2b2aa1c56397472b5c
Rahul Reddy Chavva <chavva@usf.edu>
2021-07-15 23:39:22 -0400

    Adding scope related practise work

--- Practise/function_declaration.js
+++ Practise/function_declaration.js
@@ -3,3 +3,37 @@ function greet(name){
     return `Hello ${name}`;
 }
 console.log(greet("Soujanya"));
+
+let val;
+
+val = window.outerHeight;
+val = window.outerWidth;
+
+val = window.innerHeight;
+val = window.innerWidth;
+
+val = window.location;
+val = window.location.hostname; //http://127.0.0.1
+val = window.location.port; //5500
+val = window.location.href; //http://127.0.0.1:5500/Practise/index.html
+val = window.location.search;  //http://127.0.0.1:5500/Practise/index.html?id=1
+
+// Redirect
+// window.location.href = 'http://google.com';
+// Reload
+// window.location.reload();
+
+//History Object
+
+//window.history.go(-2);
+val = window.history.length;
+
+//Navigator Object
+
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.language;
+
+console.log(val);
