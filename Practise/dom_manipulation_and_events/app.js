89a4d1d6b007345e4d7a368952a1494117e201a7
jscodebit <soujanya.janapatla@gmail.com>
2021-07-20 18:01:44 -0400

    Fininshed Javascript fundamentals

--- Practise/dom_manipulation_and_events/app.js
+++ Practise/dom_manipulation_and_events/app.js
@@ -1,36 +1,24 @@
-// EVENT BUBBLING
-
-// document.querySelector('.card-title').addEventListener('click', function(){
-//   console.log('card title');
-// });
-
-// document.querySelector('.card-content').addEventListener('click', function(){
-//   console.log('card content');
-// });
-
-// document.querySelector('.card').addEventListener('click', function(){
-//   console.log('card');
-// });
-
-// document.querySelector('.col').addEventListener('click', function(){
-//   console.log('col');
-// });
-
-// EVENT DELGATION
-
-// const delItem = document.querySelector('.delete-item');
-
-// delItem.addEventListener('click', deleteItem);
-
-document.body.addEventListener('click', deleteItem);
-
-function deleteItem(e){
-  // if(e.target.parentElement.className === 'delete-item secondary-content'){
-  //   console.log('delete item');
-  // }
-
-  if(e.target.parentElement.classList.contains('delete-item')){
-    console.log('delete item');
-    e.target.parentElement.parentElement.remove();
+// Person constructor
+function Person(name, dob) {
+  this.name = name;
+  //this.age = age;
+  this.birthday = new Date(dob);
+  this.calculateAge = function(){
+    //console.log(this.birthday.getTime());
+    const diff =  Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    console.log(ageDate);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
   }
+  // console.log(this);
 }
+
+// console.log(this);
+
+// const brad = new Person('Brad', 36);
+// const john = new Person('John', 30);
+
+// console.log(john);
+
+const brad = new Person('Brad', '9-10-1981');
+console.log(brad.calculateAge());
